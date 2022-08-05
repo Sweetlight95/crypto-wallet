15f9511257b42c4337150b290711e00e2ddb12a7
unknown <Sweetlight95>
2022-08-05 19:07:57 +0100

    wallet

---
+++ lib/ui/homeview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Home View"),
+    );
+  }
+}
